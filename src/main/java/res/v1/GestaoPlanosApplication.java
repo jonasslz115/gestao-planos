149d3ea9df3631d5b86ca22cac248ff25eb56bab
Jonas dos Santos Luiz <jonas_slz@hotmail.com>
2019-11-24 22:24:20 -0200

    inicio do projeto

---
+++ src/main/java/res/v1/GestaoPlanosApplication.java
@@ -0,0 +1,13 @@
+package res.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoPlanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoPlanosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/res/v1/GestaoPlanosApplicationTests.java
@@ -0,0 +1,13 @@
+package res.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoPlanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
